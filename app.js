e7410558fd5c3b1c65de9d3a5e0af238a06425fd
Hugo Leonel <hugoleonel.code@gmail.com>
2024-01-20 09:38:31 -0300

    Enquanto...

--- app.js
+++ app.js
@@ -2,15 +2,22 @@ alert('Boas-vindas ao jogo do Número Secreto!');
 let numeroSecreto = 5;
 console.log(numeroSecreto)
 
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute;
 
-// se chute for igual ao número secreto
-if(chute == numeroSecreto) {
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`)
-} else {
-    if(chute > numeroSecreto) {
-        alert(`O número secreto é menor que ${chute}`)
+// enquanto não for igual ao número secreto
+while (chute != numeroSecreto) {
+
+    chute = prompt('Escolha um número entre 1 e 10');
+
+    // se chute for igual ao número secreto
+    if(chute == numeroSecreto) {
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`)
     } else {
-        alert(`O número secreto é maior que ${chute}`)
+        if(chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`)
+        } else {
+            alert(`O número secreto é maior que ${chute}`)
+        }
     }
+
 }
